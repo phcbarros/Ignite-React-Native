5e09c17b949fc05f9ea8b8236d2630e0186401d8
phcbarros <paulo.barros@outlook.com.br>
2022-03-19 16:41:04 -0300

    Chapter 2 - Expo e Styled Components - Styled Components - Posicionando os cartões em destaque

--- gofinances/src/components/HighlightCard/styles.ts
+++ gofinances/src/components/HighlightCard/styles.ts
@@ -8,12 +8,12 @@ export const Container = styled.View`
   padding: ${RFValue(19)}px ${RFValue(23)}px;
   padding-bottom: ${RFValue(42)}px;
   width: ${RFValue(300)}px;
+  margin-right: 16px;
 `
 
 export const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin-right: 16px;
 `
 
 export const Title = styled.Text`
