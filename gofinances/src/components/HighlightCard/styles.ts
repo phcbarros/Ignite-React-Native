cbafb0c7c6c27bfff1524b4219366dbc835cf48c
phcbarros <paulo.barros@outlook.com.br>
2022-03-19 17:14:04 -0300

    Chapter 2 - Expo e Styled Components - Styled Components - Tipando e acessando propriedades no Styled Components

--- gofinances/src/components/HighlightCard/styles.ts
+++ gofinances/src/components/HighlightCard/styles.ts
@@ -1,9 +1,14 @@
-import styled from 'styled-components/native'
-import {RFPercentage, RFValue} from 'react-native-responsive-fontsize'
+import styled, {css} from 'styled-components/native'
+import {RFValue} from 'react-native-responsive-fontsize'
 import {Feather} from '@expo/vector-icons'
 
-export const Container = styled.View`
-  background-color: ${(props) => props.theme.colors.shape};
+interface TypeProps {
+  type: 'up' | 'down' | 'total'
+}
+
+export const Container = styled.View<TypeProps>`
+  background-color: ${({theme, type}) =>
+    type === 'total' ? theme.colors.secondary : theme.colors.shape};
   border-radius: 5px;
   padding: ${RFValue(19)}px ${RFValue(23)}px;
   padding-bottom: ${RFValue(42)}px;
@@ -16,28 +21,50 @@ export const Header = styled.View`
   justify-content: space-between;
 `
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
   font-family: ${({theme}) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
-  color: ${({theme}) => theme.colors.title};
+  color: ${({theme, type}) =>
+    type === 'total' ? theme.colors.shape : theme.colors.title};
 `
 
-export const Icon = styled(Feather)`
-  color: ${({theme}) => theme.colors.success};
+export const Icon = styled(Feather)<TypeProps>`
   font-size: ${RFValue(40)}px;
+
+  ${({type}) =>
+    type === 'up' &&
+    css`
+      color: ${({theme}) => theme.colors.success};
+    `};
+
+  ${({type}) =>
+    type === 'down' &&
+    css`
+      color: ${({theme}) => theme.colors.attention};
+    `};
+
+  ${({type}) =>
+    type === 'total' &&
+    css`
+      color: ${({theme}) => theme.colors.shape};
+    `};
 `
 
 export const Footer = styled.View``
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
   font-family: ${({theme}) => theme.fonts.medium};
   font-size: ${RFValue(32)}px;
-  color: ${({theme}) => theme.colors.textDark};
   margin-top: 38px;
+
+  color: ${({theme, type}) =>
+    type === 'total' ? theme.colors.shape : theme.colors.textDark};
 `
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<TypeProps>`
   font-family: ${({theme}) => theme.fonts.regular};
   font-size: ${RFValue(12)}px;
-  color: ${({theme}) => theme.colors.text};
+
+  color: ${({theme, type}) =>
+    type === 'total' ? theme.colors.shape : theme.colors.text};
 `
