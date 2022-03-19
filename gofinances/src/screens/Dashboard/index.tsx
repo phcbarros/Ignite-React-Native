0118cbf4e0cb83b5c99c0b00caf5ae38b28dcb22
phcbarros <paulo.barros@outlook.com.br>
2022-03-19 15:38:27 -0300

    Chapter 2 - Expo e Styled Components - Styled Components - Utilizando proporções em medidas

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import {Text} from 'react-native'
 
-import {Container, Title} from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetting,
+  UserName,
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/12138058?v=4',
+              }}
+            />
+            <User>
+              <UserGreetting>Olá,</UserGreetting>
+              <UserName>Paulo Barros</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
