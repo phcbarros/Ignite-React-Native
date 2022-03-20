d89387f4e6add26c4adda77111b2dd98ab6fb9a1
phcbarros <paulo.barros@outlook.com.br>
2022-03-20 16:05:21 -0300

    Chapter 2 - Formulários e Navegação - Formulários - Componente de Botão

--- gofinances/src/screens/Register/styles.ts
+++ gofinances/src/screens/Register/styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native'
-import {RFPercentage, RFValue} from 'react-native-responsive-fontsize'
-import {Feather} from '@expo/vector-icons'
+import {RFValue} from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
   flex: 1;
@@ -28,7 +27,10 @@ export const Title = styled.Text`
 
 export const Form = styled.View`
   flex: 1;
+  justify-content: space-between;
   width: 100%;
 
   padding: 24px;
 `
+
+export const Fields = styled.View``
