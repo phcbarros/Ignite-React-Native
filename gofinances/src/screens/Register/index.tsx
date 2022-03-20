3fd1f433a0f0f266af672bdc06250e13e5758c7d
phcbarros <paulo.barros@outlook.com.br>
2022-03-20 17:26:43 -0300

    Chapter 2 - Formulários e Navegação - Formulários - Componente Select

--- gofinances/src/screens/Register/index.tsx
+++ gofinances/src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Button} from '../../components/Form/Button'
+import {CategorySelect} from '../../components/Form/CategorySelect'
 
 import {Input} from '../../components/Form/Input'
 import {TransactionTypeButton} from '../../components/Form/TransactionTypeButton'
@@ -50,6 +51,8 @@ export function Register() {
               isActive={transactionType === EnumTransactionsType.down}
             />
           </TransactionsType>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
         <Button title="Enviar" />
