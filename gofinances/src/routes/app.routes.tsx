44e3ddf30d37a8757c6525f4973ed66e76d6deed
phcbarros <paulo.barros@outlook.com.br>
2022-03-26 17:10:08 -0300

    Chapter 2 - Formulários e Navegação - Navegação - Estilizando a Tab Navigation

--- gofinances/src/routes/app.routes.tsx
+++ gofinances/src/routes/app.routes.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import {Platform} from 'react-native'
+import {useTheme} from 'styled-components'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 import {Dashboard} from '../screens/Dashboard'
 import {Register} from '../screens/Register'
@@ -7,11 +10,51 @@ import {Register} from '../screens/Register'
 const {Navigator, Screen} = createBottomTabNavigator()
 
 export function AppRoutes() {
+  const theme = useTheme()
+
   return (
-    <Navigator>
-      <Screen name="Dashboard" component={Dashboard} />
-      <Screen name="Register" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        },
+      }}>
+      <Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   )
 }
