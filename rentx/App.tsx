a919e9abab22f17b7faed71eb79269d4a94ee3fa
phcbarros <paulo.barros@outlook.com.br>
2022-07-18 15:21:56 -0300

    Chapter 3 - Consumindo API - Tema Global da Aplicação

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -1,5 +1,62 @@
+import React, {useEffect, useState, useCallback} from 'react'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo'
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+import {ThemeProvider} from 'styled-components'
+
 import {Home} from './src/screens/Home'
 
+import theme from './src/styles/theme'
+import {View} from 'react-native'
+
 export default function App() {
-  return <Home />
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+        await Font.loadAsync({
+          Archivo_400Regular,
+          Archivo_500Medium,
+          Archivo_600SemiBold,
+          Inter_400Regular,
+          Inter_500Medium,
+        })
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
+  return (
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1,
+      }}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </View>
+  )
 }
