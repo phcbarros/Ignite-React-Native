5d8a75a2881ff89c48aa5d4bd2e938daf87b632d
phcbarros <paulo.barros@outlook.com.br>
2022-07-18 15:52:09 -0300

    Chapter 3 - Criação das Interfaces - Criação do Header da Home

--- rentx/src/screens/Home/styles.ts
+++ rentx/src/screens/Home/styles.ts
@@ -1,11 +1,29 @@
+import {RFValue} from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({theme}) => theme.colors.background_primary};
 `
 
-export const Title = styled.Text``
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({theme}) => theme.colors.header};
+  padding: 32px 24px;
+  justify-content: flex-end;
+`
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme}) => theme.fonts.primary_400};
+  color: ${({theme}) => theme.colors.text};
+`
