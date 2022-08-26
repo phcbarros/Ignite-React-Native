5b29f8dc9740e3ab4efe2e3364d6e5824d47b6f9
phcbarros <paulo.barros@outlook.com.br>
2022-08-26 19:17:01 -0300

    Chapter 3 - Animações - Instalação Reanimated

--- rentx/src/routes/stack.routes.tsx
+++ rentx/src/routes/stack.routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 
+import {Splash} from '../screens/Splash'
 import {Home} from '../screens/Home'
 import {CarDetails} from '../screens/CarDetails'
 import {Scheduling} from '../screens/Scheduling'
@@ -11,6 +12,7 @@ import {MyCars} from '../screens/MyCars'
 import {CarDTO} from '../dtos/CarDTO'
 
 export type AppRoutes = {
+  Splash: undefined
   Home: undefined
   CarDetails: {car: CarDTO}
   Scheduling: {car: CarDTO}
@@ -29,7 +31,8 @@ const {Navigator, Screen} = createStackNavigator<AppRoutes>()
 
 export function StackRoutes() {
   return (
-    <Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+    <Navigator screenOptions={{headerShown: false}} initialRouteName="Splash">
+      <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
---
+++ rentx/src/screens/Splash/index.tsx
@@ -0,0 +1,15 @@
+import {StatusBar} from 'react-native'
+
+import {Container} from './styles'
+
+export function Splash() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </Container>
+  )
+}
