806d62b3983335df575acad89e2e725b27a5cec1
phcbarros <paulo.barros@outlook.com.br>
2022-08-31 17:47:36 -0300

    Chapter 3 - Animações - Primeira Animação

--- rentx/src/screens/Splash/index.tsx
+++ rentx/src/screens/Splash/index.tsx
@@ -1,8 +1,32 @@
 import {StatusBar} from 'react-native'
+import {Button, StyleSheet} from 'react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated'
 
 import {Container} from './styles'
 
+//useSharedValue compartilha valores entre as animações
+//useAnimatedStyle usado para mudar os estilos durante as animações
+
 export function Splash() {
+  const animation = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: animation.value,
+        },
+      ],
+    }
+  })
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 100
+  }
+
   return (
     <Container>
       <StatusBar
@@ -10,6 +34,17 @@ export function Splash() {
         backgroundColor="transparent"
         translucent
       />
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+})
